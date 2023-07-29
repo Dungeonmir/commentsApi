b3ca3b7acfc5ec88cd259259d58a2d75e4de372d
Sergey <dungeonmir@gmail.com>
2023-07-29 11:43:47 +0300

    add Error Handler class, add simple examples for methods

---
+++ examples/post.php
@@ -0,0 +1,7 @@
+<?php
+include './classes/Api.class.php';
+
+header("Content-type: application/json; charset=utf-8");
+$api = new Api("https://jsonplaceholder.typicode.com/");
+
+echo $api->getPost(9);
